ca20c540676d234d56669e1250cd29a467024c7e
EnglishMagik <chantelhere@gmail.com>
2026-04-21 15:00:50 +0200

    latest changest

--- src/pages/DedicationPage.jsx
+++ src/pages/DedicationPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-data'
+import { useNavigate } from 'react-router-dom'
 import { useBookStore } from '../store/bookStore'
 
 export default function DedicationPage() {
